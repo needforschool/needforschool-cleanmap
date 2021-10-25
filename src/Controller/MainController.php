9529e4cf16202693728dff06dc0c1efc53fa3791
Antoine Kingue <etorefr@gmail.com>
2021-10-25 10:29:32 +0200

    🎨 improve main controller by adding react routing

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'main')]
+    #[Route('/{reactRouting}', name: 'main', requirements: ['reactRouting' => ".+"], defaults: ['reactRouting' => null])]
     public function index(): Response
     {
         return $this->render('base.html.twig');
