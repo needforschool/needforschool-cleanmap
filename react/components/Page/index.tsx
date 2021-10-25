01a5b4caea8e97053c2fdb135db3ddd6f475e3ef
Antoine Kingue <etorefr@gmail.com>
2021-10-25 15:17:57 +0200

    ✨ add buttons to page

--- react/components/Page/index.tsx
+++ react/components/Page/index.tsx
@@ -1,18 +1,21 @@
 import styled from "styled-components";
+import Button from "../Layout/Button";
 import Sidebar from "../Sidebar";
 
 interface Props {
   title: string;
   children: React.ReactNode;
+  buttons?: React.ReactNode;
 }
 
-const Page: React.FC<Props> = ({ title, children }: Props) => {
+const Page: React.FC<Props> = ({ title, children, buttons }: Props) => {
   return (
     <Container>
       <Sidebar />
       <Main>
         <Header>
           <Title>{title}</Title>
+          {buttons && <ButtonContainer>{buttons}</ButtonContainer>}
         </Header>
         <Content>{children}</Content>
       </Main>
@@ -51,6 +54,19 @@ export const Content = styled.div`
   flex: 1;
   flex-direction: column;
   overflow: auto;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  ${Button} {
+    margin: 0 5px;
+    :first-child {
+      margin-left: 0;
+    }
+    :last-child {
+      margin-right: 0;
+    }
+  }
 `;
 
 export default Page;
