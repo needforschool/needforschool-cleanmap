dfccd45861a4d68fe7322153ccd024a7e74cece7
Antoine Kingue <etorefr@gmail.com>
2021-10-25 10:48:12 +0200

    ✨ add map screen

---
+++ react/screens/Map/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Page from "../../components/Page";
+
+const Map: React.FC = () => {
+  return (
+    <Page title={"Map"}>
+      <Container>Screen Children</Container>
+    </Page>
+  );
+};
+
+const Container = styled.div``;
+
+export default Map;
